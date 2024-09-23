89003dd592fcc4d89a820f4819501d9c90f12bd5
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-09-23 19:02:03 -0300

    subo taller 4 incompleto

---
+++ taller 4/StackAr/src/main/java/org/autotest/mutantGenerator/operators/conditionals/TrueConditionalsMutator.java
@@ -0,0 +1,39 @@
+package org.autotest.mutantGenerator.operators.conditionals;
+
+import org.autotest.mutantGenerator.operators.MutationOperator;
+import spoon.reflect.code.BinaryOperatorKind;
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtIf;
+import spoon.reflect.declaration.CtElement;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Operador de mutación basado en https://pitest.org/quickstart/mutators/#REMOVE_CONDITIONALS
+ *
+ * Este operador reemplaza los valores en las condiciones de guardas por true.
+ */
+public class TrueConditionalsMutator extends MutationOperator {
+    @Override
+    public boolean isToBeProcessed(CtElement candidate) {
+        // llamo al método super para verificar que el candidato no es parte de randoop.CheckRep: repOK method
+        if (!super.isToBeProcessed(candidate)) {
+            return false;
+        }
+        return candidate instanceof CtIf;
+    }
+
+    @Override
+    public void process(CtElement candidate) {
+
+    }
+
+    @Override
+    public String describeMutation(CtElement candidate) {
+        CtIf op = (CtIf)candidate;
+        return this.getClass().getSimpleName() + ": Se reemplazó " +
+                op.getCondition().toString() + " por true"+
+                " en la línea " + op.getPosition().getLine() + ".";
+    }
+}

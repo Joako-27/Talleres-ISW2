2adcbd6c9880ae2967dc7b6906432925b076939d
Joaco <joaquin.e.munoz@gmail.com>
2024-09-25 13:25:02 -0300

    Agregados algunos tests. Faltan matar al menos 10 mutantes. Mutantes equivalentes añadidos a respuestas

--- taller 4/StackAr/src/test/java/org/autotest/StackTests3.java
+++ taller 4/StackAr/src/test/java/org/autotest/StackTests3.java
@@ -62,4 +62,73 @@ public class StackTests3 extends MutationAnalysisRunner {
     }
 
     // COMPLETAR
+
+    public void testPop() throws Exception{
+        Stack stack = createStack(2);
+        stack.push(1);
+        stack.push(3);
+        assertEquals(3, stack.pop());
+        assertEquals("[1]", stack.toString());
+        assertEquals(1, stack.top());
+        assertEquals(1, stack.size());
+        assertEquals(1, stack.pop());
+        assertEquals(0, stack.size());
+        assertTrue(stack.isEmpty());
+        assertFalse(stack.isFull());
+        assertThrows(IllegalStateException.class, () -> {stack.pop();});
+    }
+
+    public void testPush() throws Exception{
+        Stack stack = createStack(2);
+        stack.push(1);
+        assertFalse(stack.isEmpty());
+        assertFalse(stack.isFull());
+        assertEquals(1, stack.top());
+        stack.push(3);
+        assertTrue(stack.isFull());
+        assertEquals(3, stack.top());
+        assertThrows(IllegalStateException.class, () -> {stack.push(2);});
+    }
+
+    public void testEquals() throws Exception{
+        Stack stack1 = createStack(10);
+        stack1.push(13);
+        stack1.push(14);
+        stack1.push(11);
+        stack1.push(15);
+        Stack stack2 = createStack(10);
+        stack2.push(13);
+        stack2.push(14);
+        stack2.push(11);
+        stack2.push(15);
+        assertTrue(stack1.equals(stack2));
+        stack1.pop();
+        assertFalse(stack2.equals(stack1));
+        Stack stack3 = createStack(9);
+        stack2.push(13);
+        stack2.push(14);
+        stack2.push(11);
+        assertFalse(stack1.equals(stack3));
+        Stack stack4 = null;
+        assertFalse(stack1.equals(stack4));
+        int a = 5;
+        assertFalse(stack1.equals(a));
+        assertTrue(stack1.equals(stack1));
+        Stack stack5 = createStack(0);
+        assertFalse(stack1.equals(stack5));
+    }
+
+    public void testHashcode() throws Exception{
+        Stack stack1 = createStack();
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        Stack stack2 = createStack();
+        stack2.push(1);
+        stack2.push(2);
+        stack2.push(3);
+        assertEquals(stack1.hashCode(), stack2.hashCode());
+        stack1.pop();
+        assertNotEquals(stack1.hashCode(), stack2.hashCode());
+    }
 }

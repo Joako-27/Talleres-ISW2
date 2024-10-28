46d57b74369442f41152b5d819e34ccfb8daf146
cchaves <cchaves@dc.uba.ar>
2024-10-28 16:22:42 -0300

    ej3 y 4 incompletos

--- taller_6/CgiDecode/src/get_fitness_cgi_decode.py
+++ taller_6/CgiDecode/src/get_fitness_cgi_decode.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from src.evaluate_condition import clear_maps
-
+from src.cgi_decode_instrumented import cgi_decode_instrumented
+from src.evaluate_condition import distances_false,distances_true
 
 def get_fitness_cgi_decode(test_suite: List[str]) -> float:
     # Borro la información de branch coverage de ejecuciones anteriores
@@ -9,5 +10,24 @@ def get_fitness_cgi_decode(test_suite: List[str]) -> float:
     clear_maps()
 
     fitness = 0
-    # TODO: COMPLETAR
-    return 0
+    for test in test_suite:
+        try:
+            cgi_decode_instrumented(test)
+            for key in distances_false:
+                d_false = distances_false[key]
+                d_true = distances_true[key]
+                if(d_false != 0):
+                    d_false = d_false/(d_false+1)
+                else: 
+                    d_false = 1
+                if(d_true != 0):
+                    d_true = d_true/(d_true+1)
+                else:
+                    d_true = 1
+                fitness += d_true
+                fitness += d_false
+
+
+        except:
+            continue
+    return fitness
--- taller_6/CgiDecode/test/test_evaluate_condition_for_cgi_decode_instrumented.py
+++ taller_6/CgiDecode/test/test_evaluate_condition_for_cgi_decode_instrumented.py
@@ -2,13 +2,13 @@
 import unittest
 from src.cgi_decode import cgi_decode
 from src.cgi_decode_instrumented import cgi_decode_instrumented
-from src.evaluate_condition import clear_maps, get_true_distance, get_false_distance
+from src.evaluate_condition import clear_maps, get_true_distance, get_false_distance,distances_false,distances_true
 
 
 class TestEvaluateConditionForCgiDecodeInstrumented(unittest.TestCase):
-    def testExample(self):
-        # TODO COMPLETAR
-        cgi_decode_instrumented("Hello+Reader")
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(True, False)
+    def test_1(self):
+        clear_maps()
+        res = cgi_decode_instrumented("test+uno")
+        self.assertEqual(res, "test uno")
+        self.assertEqual(distances_true, {1: 0, 2: 0, 3: 64})
+        self.assertEqual(distances_false, {1: 0, 2: 0, 3: 0})

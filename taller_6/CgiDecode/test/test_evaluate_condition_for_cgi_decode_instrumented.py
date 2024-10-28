e841bbf80331e9f5748c5e47bb1e92c749791ee8
cchaves <cchaves@dc.uba.ar>
2024-10-28 16:46:36 -0300

    3b

--- taller_6/CgiDecode/test/test_evaluate_condition_for_cgi_decode_instrumented.py
+++ taller_6/CgiDecode/test/test_evaluate_condition_for_cgi_decode_instrumented.py
@@ -12,3 +12,9 @@ class TestEvaluateConditionForCgiDecodeInstrumented(unittest.TestCase):
         self.assertEqual(res, "test uno")
         self.assertEqual(distances_true, {1: 0, 2: 0, 3: 64})
         self.assertEqual(distances_false, {1: 0, 2: 0, 3: 0})
+    def test_2(self):
+        clear_maps()
+        res = cgi_decode_instrumented("h%69")
+        self.assertEqual(res,"hi")
+        self.assertEqual(distances_true,{1: 0, 2: 6, 3: 0, 4: 0, 5: 0})
+        self.assertEqual(distances_false,{1: 0, 2: 0, 3: 0, 4: 1, 5: 1})

089881f4e433e438f54df0b9b6722295f1c661ea
Juan Begalli <juanbegalli@gmail.com>
2024-10-28 13:32:21 -0300

    ej1

--- taller_6/CgiDecode/test/test_cgi_decode.py
+++ taller_6/CgiDecode/test/test_cgi_decode.py
@@ -4,9 +4,21 @@ from src.cgi_decode import cgi_decode
 
 
 class TestCgiDecode(unittest.TestCase):
-    def testExample(self):
-        # TODO COMPLETAR
-        cgi_decode("Hello World")
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(True, False)
+    def test_mas_espacio(self):
+        self.assertEqual(cgi_decode("Hola+mundo"),"Hola mundo")
+        #self.assertEqual(cgi_decode("oracion+mas+larga+para+probar"), "oracion mas larga para probar")
+    
+    def test_porcentaje(self):
+        self.assertEqual(cgi_decode("hola%2cinge2%2ctesteo+comas"), "hola,inge2,testeo comas")
+        self.assertEqual(cgi_decode("%6F%74%72%6F%73+%63%61%72%61%63%74%65%72%65%73"), "otros caracteres")
+    
+    def test_texto_sin_codificar(self):
+        self.assertEqual(cgi_decode("SinNada"),"SinNada")
+
+    def test_invalid_high_digit(self):
+        with self.assertRaises(ValueError):
+            cgi_decode("primeroInvalido%H2")
+    
+    def test_invalid_low_digit(self):
+        with self.assertRaises(ValueError): 
+            cgi_decode("segundoInvalido%2P")
